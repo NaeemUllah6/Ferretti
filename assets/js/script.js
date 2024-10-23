c24c37d675594a04e6e7c5ae5e3c1c28a8ceb0fd
NaeemUllah6 <naeemullahkhokhar6@gmail.com>
2024-10-23 20:00:39 +0500

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+    // --------------------script for navbar--------------------
+    document.querySelector('.navbar-toggler').addEventListener('click', function () {
+        const navbarCollapse = document.querySelector('.navbar-collapse');
+        navbarCollapse.classList.toggle('active');
+    });
